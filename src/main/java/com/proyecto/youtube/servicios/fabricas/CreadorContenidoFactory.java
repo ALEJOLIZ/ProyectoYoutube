0ab8e745817f63c7a5baff59f095d0b475a35722
Julian <alvarezortizjulian@gmail.com>
2026-05-19 10:11:58 -0500

    Correccion logica codigos fabrica

--- src/main/java/com/proyecto/youtube/servicios/fabricas/CreadorContenidoFactory.java
+++ src/main/java/com/proyecto/youtube/servicios/fabricas/CreadorContenidoFactory.java
@@ -1,6 +1,6 @@
 package com.proyecto.youtube.servicios.fabricas;
 
-import com.proyecto.youtube.modelo.usuario.CreadorContenido;
+import com.proyecto.youtube.modelo.usuario.canal.Canal;
 import com.proyecto.youtube.modelo.contenido.Contenido;
 import com.proyecto.youtube.modelo.contenido.TransmisionEnVivo;
 import com.proyecto.youtube.modelo.contenido.Short;
@@ -11,16 +11,16 @@ public class CreadorContenidoFactory implements  FabricaContenido{
     public Contenido crearContenido(TipoContenido tipo, String titulo, String descripcion, CreadorContenido autor, Object... args) {
         switch (tipo){
             case VIDEO_LARGO:
-                int duracion=(Integer) args[0];
-                boolean monetizado =(Boolean) args[1];
-                return new VideoLargo(titulo,descripcion,autor,duracion,monetizado);
+                int duracion=(args.length > 0 && args[0] instanceof Integer)?(Integer) args[0]: 0;
+                boolean monetizado =(args.length > 1 && args[1] instanceof Boolean) ? (Boolean) args[1] : false;;
+                return new VideoLargo(titulo,descripcion,canalAutor,duracion,monetizado);
 
             case SHORT:
-                String pistaAudio=(String)  args[0];
-                return new Short(titulo,descripcion,autor,pistaAudio);
+                String pistaAudio=(args.length > 0 && args[0] instanceof String) ? (String) args[0] : "";
+                return new Short(titulo,descripcion,canalAutor,pistaAudio);
 
             case TRANSMISION_EN_VIVO:
-                return new TransmisionEnVivo(titulo,descripcion,autor);
+                return new TransmisionEnVivo(titulo,descripcion,canalAutor);
 
 
             default:
--- src/main/java/com/proyecto/youtube/servicios/fabricas/FabricaContenido.java
+++ src/main/java/com/proyecto/youtube/servicios/fabricas/FabricaContenido.java
@@ -1,7 +1,7 @@
 package com.proyecto.youtube.servicios.fabricas;
 
-import com.proyecto.youtube.modelo.usuario.CreadorContenido;
+import com.proyecto.youtube.modelo.usuario.canal.Canal;
 import com.proyecto.youtube.modelo.contenido.Contenido;
 public interface FabricaContenido {
-    Contenido crearContenido(TipoContenido tipo, String titulo,String descripcion, CreadorContenido autor, Object... args);
+    Contenido crearContenido(TipoContenido tipo, String titulo,String descripcion, Canal canalAutor, Object... args);
 }
