67f3ce9c26b552c682b5bcf60235145b5409571c
carlos-rosado <carlosalbertorosado1412@gmail.com>
2026-05-18 23:26:33 -0500

    reemplazo herencia de usuarios por modelo Usuario-Canal

--- src/main/java/com/proyecto/youtube/modelo/contenido/Contenido.java
+++ src/main/java/com/proyecto/youtube/modelo/contenido/Contenido.java
@@ -1,7 +1,5 @@
 package com.proyecto.youtube.modelo.contenido;
 
-import com.proyecto.youtube.modelo.usuario.CreadorContenido;
-
 public abstract class Contenido {
     private java.util.UUID id;
     private CreadorContenido autor;
