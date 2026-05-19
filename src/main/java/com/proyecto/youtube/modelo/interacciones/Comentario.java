41952fced72c18548ede2baecc7c174c034717cc
miconra <miguelangelconra@gmail.com>
2026-05-18 23:31:47 -0500

    clase comentario

---
+++ src/main/java/com/proyecto/youtube/modelo/interacciones/Comentario.java
@@ -0,0 +1,31 @@
+package com.proyecto.youtube.modelo.interacciones;
+import com.proyecto.youtube.modelo.usuario.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comentario {
+    private String texto;
+    private List<Comentario> respuestas;
+
+    public Comentario(Usuario autor, String texto){
+        super();
+        this.texto = texto;
+        this.respuestas = new ArrayList<>();
+    }
+
+    public void agregarRespuesta(Comentario respuesta) {
+        if (respuesta != null) {
+            this.respuestas.add(respuesta);
+        }
+    }
+
+    public String getTexto() { return texto; }
+    public List<Comentario> getRespuestas() { return new ArrayList<>(respuestas); }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Comentó: \"" + texto + "\" (" + respuestas.size() + " respuestas)";
+    }
+
+}
