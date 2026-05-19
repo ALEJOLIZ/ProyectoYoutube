34a144533a58baf3515504f272e44b92590b9576
Julian <alvarezortizjulian@gmail.com>
2026-05-18 20:20:00 -0500

    Creacion de CreadorContenidoFactory, FabricaContenido y TipoContenido

---
+++ src/main/java/com/proyecto/youtube/servicios/fabricas/CreadorContenidoFactory.java
@@ -0,0 +1,30 @@
+package com.proyecto.youtube.servicios.fabricas;
+
+import com.proyecto.youtube.modelo.usuario.CreadorContenido;
+import com.proyecto.youtube.modelo.contenido.Contenido;
+import com.proyecto.youtube.modelo.contenido.TransmisionEnVivo;
+import com.proyecto.youtube.modelo.contenido.Short;
+import com.proyecto.youtube.modelo.contenido.VideoLargo;
+public class CreadorContenidoFactory implements  FabricaContenido{
+
+    @Override
+    public Contenido crearContenido(TipoContenido tipo, String titulo, String descripcion, CreadorContenido autor, Object... args) {
+        switch (tipo){
+            case VIDEO_LARGO:
+                int duracion=(Integer) args[0];
+                boolean monetizado =(Boolean) args[1];
+                return new VideoLargo(titulo,descripcion,autor,duracion,monetizado);
+
+            case SHORT:
+                String pistaAudio=(String)  args[0];
+                return new Short(titulo,descripcion,autor,pistaAudio);
+
+            case TRANSMISION_EN_VIVO:
+                return new TransmisionEnVivo(titulo,descripcion,autor);
+
+
+            default:
+                throw new IllegalArgumentException("Tipo de contenido no reconocido.");
+        }
+    }
+}
--- src/main/java/com/proyecto/youtube/servicios/fabricas/FabricaContenido.java
+++ src/main/java/com/proyecto/youtube/servicios/fabricas/FabricaContenido.java
@@ -1,4 +1,7 @@
 package com.proyecto.youtube.servicios.fabricas;
 
+import com.proyecto.youtube.modelo.usuario.CreadorContenido;
+import com.proyecto.youtube.modelo.contenido.Contenido;
 public interface FabricaContenido {
+    Contenido crearContenido(TipoContenido tipo, String titulo,String descripcion, CreadorContenido autor, Object... args);
 }
