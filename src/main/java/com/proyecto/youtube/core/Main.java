7b7bd59c940a88b3f21edde6a0e1d80ee828187c
ALEJOLIZ <ajejo.lizcano@gmail.com>
2026-05-20 20:56:13 -0500

    Escenario 4, 5, 6

--- src/main/java/com/proyecto/youtube/core/Main.java
+++ src/main/java/com/proyecto/youtube/core/Main.java
@@ -1,5 +1,8 @@
 package com.proyecto.youtube.core;
 
+import com.proyecto.youtube.modelo.interacciones.Comentario;
+import com.proyecto.youtube.modelo.interacciones.TipoReaccion;
+import com.proyecto.youtube.servicios.fabricas.TipoContenido;
 import com.proyecto.youtube.servicios.recomendaciones.RecomendadorPorTendencias;
 
 import java.util.UUID;
@@ -31,6 +34,45 @@ public class Main {
         youtube.suscribirUsuarioACanal(idElena, canalCarlos);
         youtube.suscribirUsuarioACanal(idProfesor, canalCarlos);
         youtube.suscribirUsuarioACanal(idCarlos, canalElena);
+        System.out.println();
+
+        // ESCENARIO 4: Publicación de Contenido
+        System.out.println(">> ESCENARIO 4: Publicación de Contenido (Factory)");
+        UUID idVideoLargo = youtube.publicarContenido(
+                canalCarlos, TipoContenido.VIDEO_LARGO,
+                "Guía de Arquitectura Clean Code", "Tutorial de POO", 1200, true
+        );
+
+        UUID idStream = youtube.publicarContenido(
+                canalElena, TipoContenido.TRANSMISION_EN_VIVO,
+                "Viaje a Tokyo en Vivo", "¡Acompáñenme!", true, 0
+        );
+        System.out.println();
+
+        // ESCENARIO 5: VISUALIZACIONES E INTERACCIONES
+        System.out.println(">> ESCENARIO 5: Visualizaciones e Interacciones");
+
+        youtube.verContenido(idElena, idVideoLargo);
+        youtube.verContenido(idProfesor, idVideoLargo);
+
+        youtube.reaccionarAContenido(idElena, idVideoLargo, TipoReaccion.LIKE);
+        youtube.reaccionarAContenido(idCarlos, idVideoLargo, TipoReaccion.DISLIKE);
+
+        Comentario comentarioProfesor = youtube.comentarContenido(idProfesor, idVideoLargo, "Excelente explicación sobre los patrones de diseño, muy útil.");
+        youtube.responderComentario(idCarlos, comentarioProfesor, "¡Muchas gracias, doctor!", idVideoLargo);
+        System.out.println();
+
+        System.out.println(">> ESCENARIO 5.1: Desglose de Estadísticas");
+        youtube.mostrarDetallesEInteracciones(idVideoLargo);
+        System.out.println();
+
+        // ESCENARIO 6: GESTIÓN DE TRANSMISIÓN EN VIVO
+        System.out.println(">> ESCENARIO 6: Estado de Transmisiones en Vivo");
+        // Se conectan usuarios al stream
+        youtube.actualizarEspectadoresEnVivo(idStream, 45);
+        youtube.actualizarEspectadoresEnVivo(idStream, 120);
+        // El creador finaliza el stream
+        youtube.finalizarTransmisionEnVivo(idStream);
         System.out.println();
 
         System.out.println("\n==========================================================");
